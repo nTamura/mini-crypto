096d34d3ba00f2aee4d608ce14158f314c0a1ec4
Nic Tamura <nictamura@gmail.com>
2019-01-10 18:43:43 -0800

    clean up clogs, fix minor styling

--- src/components/Views/Main/Container.jsx
+++ src/components/Views/Main/Container.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-// import Toolbar from 'components/Common/Toolbar'
 import NewsBody from 'components/Common/NewsBody'
 import Loading from 'components/Common/Loading'
 import ShowMore from 'components/Common/ShowMore'
@@ -8,22 +7,20 @@ import ShowMore from 'components/Common/ShowMore'
 const API_KEY = process.env.REACT_APP_NEWS_API_KEY
 // const SOURCE = 'https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey='
 
-
 class Container extends Component {
   constructor(props) {
     super(props)
     this.state = {
       isLoading: true,
-      userInput: '',
       newsData: [],
       rowsToDisplay: 5,
-      anchorEl: null,
     }
   }
 
   componentDidMount() {
     this.getNews()
-    setInterval(() => { this.getNews() }, 180000)
+    setInterval(() => { this.getNews() }, 600000)
+    // 10 minute fetch intervals
   }
 
   showMore = () => {
@@ -34,7 +31,6 @@ class Container extends Component {
   }
 
   getNews = () => {
-    // const url = SOURCE + API_KEY
     const query = 'crypto OR cryptocurrency OR blockchain OR bitcoin OR ethereum OR litecoin OR ripple OR btc OR ltc OR xrp OR eth OR bch OR EOS'
     const date = new Date().toLocaleDateString('en-CA')
     const url = `https://newsapi.org/v2/everything?language=en&q=${query}&from=${date}&sortBy=popularity&apiKey=${API_KEY}`
@@ -44,8 +40,7 @@ class Container extends Component {
         console.log(error)
       })
       .then(res => {
-        console.log(url)
-        console.log(res.data.articles)
+        // console.log(res.data.articles)
         this.setState({ newsData: res.data.articles }, () => {
           this.setState({ isLoading: false })
         })
@@ -53,11 +48,7 @@ class Container extends Component {
   }
 
   render() {
-    const {
-      isLoading, newsData, anchorEl, currency,
-      userInput, rowsToDisplay, showMore
-    } = this.state
-
+    const { isLoading, newsData, rowsToDisplay } = this.state
     return (
       <>
         { isLoading
