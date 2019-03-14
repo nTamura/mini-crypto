f261d0c9e2483c9becf8734f26269ffa2af04e32
Nic Tamura <nictamura@gmail.com>
2019-03-14 13:40:58 -0700

    about

--- src/components/Views/About.jsx
+++ src/components/Views/About.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
-import { Card, CardContent, Typography, Divider } from '@material-ui/core'
+import { Card, CardContent, Typography } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 import { InfoOutlined } from '@material-ui/icons'
 import ExtLink from 'components/Common/ExtLink'
+
+const About = ({ classes }) => (
+  <div className={classes.root}>
+    <div className={classes.titleContainer}>
+      <InfoOutlined className={classes.icon} />
+      <Typography variant="h6">About Mini-Crypto</Typography>
+    </div>
+
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="h6" inline>
+          Mini-Crypto
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="caption"
+          className={classes.sub}
+          inline
+        >
+          Alpha-0.0.2
+        </Typography>
+        <ExtLink href="https://github.com/nTamura/mini-crypto" paragraph>
+          Github Repo
+        </ExtLink>
+        <Typography>Material UI Library</Typography>
+        <ExtLink url="https://material-ui.com/" paragraph>
+          Material UI
+        </ExtLink>
+        <Typography>Cryptocompare News/Market API</Typography>
+        <ExtLink url="https://cryptocompare.com/" paragraph>
+          NewsAPI
+        </ExtLink>
+        <Typography>Cryptocoins Icons Repository</Typography>
+        <ExtLink url="https://github.com/allienworks/cryptocoins" gutterBottom>
+          AllienWorks/Cryptocoins Repo
+        </ExtLink>
+      </CardContent>
+    </Card>
+  </div>
+)
 
 const styles = {
   root: {
@@ -13,55 +53,14 @@ const styles = {
     alignItems: 'center',
     padding: '14px 0',
   },
+  sub: {
+    verticalAlign: 'middle',
+    paddingLeft: 5,
+  },
   icon: {
     color: '#048dd2',
     paddingRight: 10,
   },
-  link: {
-    color: '#FFFFFF',
-  },
 }
 
-const About = ({ classes }) => (
-  <div className={classes.root}>
-    <div className={classes.titleContainer}>
-      <InfoOutlined className={classes.icon} />
-      <Typography variant="h6">About</Typography>
-    </div>
-
-    <Card className={classes.card}>
-      <CardContent>
-        <Typography variant="h6" className={classes.title}>
-          Mini-Crypto
-        </Typography>
-        <Typography color="textSecondary" variant="caption">
-          Version alpha-0.1
-        </Typography>
-        <ExtLink href="https://github.com/nTamura/mini-crypto" paragraph>
-          Github Repo
-        </ExtLink>
-
-        <Typography>Material UI Library</Typography>
-        <ExtLink url="https://material-ui.com/" paragraph>
-          Material UI
-        </ExtLink>
-
-        <Typography>Cryptocoins Market API</Typography>
-        <ExtLink url="https://coinmarketcap.com/" paragraph>
-          Coinmarketcap
-        </ExtLink>
-
-        <Typography>Cryptocoins News API</Typography>
-        <ExtLink url="https://newsapi.org/" paragraph>
-          NewsAPI
-        </ExtLink>
-
-        <Typography>Cryptocoins Icons Repository</Typography>
-        <ExtLink url="https://github.com/allienworks/cryptocoins" gutterBottom>
-          Cryptocoins Icons
-        </ExtLink>
-      </CardContent>
-    </Card>
-  </div>
-)
 export default withStyles(styles)(About)
